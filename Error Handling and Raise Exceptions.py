56a6aa9a9035c2771fce4a8109acb680cf2bb74a
Joseph C. Richardson <34896540+Cozmo1@users.noreply.github.com>
2020-04-04 10:06:38 -0400

    Update Error Handling and Raise Exceptions.py

--- Error Handling and Raise Exceptions.py
+++ Error Handling and Raise Exceptions.py
@@ -75,15 +75,3 @@ else:
      pass
 finally:
      pass
-
-try:
-      f=open('Car_game.py')
-except FileNotFoundError as e:
-     print(e)
-except Exception as e:
-     print(e)
-else:
-     print(f.read())
-     f.close()
-finally:
-     print('Executing Finally...')
